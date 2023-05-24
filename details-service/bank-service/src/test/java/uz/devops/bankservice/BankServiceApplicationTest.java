380876a616b2b2adf7c8ce4b9c81a774ac16f747
Madiynabonu <otkirbekovamadinabonu@gmail.com>
2023-05-24 23:06:54 +0500

    done

---
+++ details-service/bank-service/src/test/java/uz/devops/bankservice/BankServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package uz.devops.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
